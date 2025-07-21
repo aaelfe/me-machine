9e97d202c7903ad82de971b7bbea91225569bc93
Allan Elfe <aaelfe@crimson.ua.edu>
2025-07-20 23:12:36 -0500

    New iOS app

---
+++ iOS Client/iOS Client/iOS_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ClientApp.swift
+//  iOS Client
+//
+//  Created by Allan Elfe on 7/20/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS Client/iOS ClientTests/iOS_ClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ClientTests.swift
+//  iOS ClientTests
+//
+//  Created by Allan Elfe on 7/20/25.
+//
+
+import Testing
+@testable import iOS_Client
+
+struct iOS_ClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
